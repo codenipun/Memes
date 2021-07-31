638b43813bbf7ef7dec6944f178a3ea7cf1b1b36
nipunjain <nipunjain119@gmail.com>
2021-07-31 21:39:01 +0530

    final commit

--- app/src/main/java/com/example/android/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/android/memeshare/MainActivity.kt
@@ -2,20 +2,20 @@ package com.example.android.memeshare
 
 import android.content.Intent
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.sql.DataSource
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,7 +44,13 @@ class MainActivity : AppCompatActivity() {
                         return false
                     }
 
-                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: com.bumptech.glide.load.DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
                         progressbar.visibility = View.GONE
                         return false
                     }
